2cb8f09d358cb5ffaa8aeae35fceb77bd13da0bc
thulanigamtee <thulanigamtee@gmail.com>
2024-11-03 16:57:54 +0200

    updated invoice info component

--- src/app/components/invoice/invoice-info/invoice-info.component.ts
+++ src/app/components/invoice/invoice-info/invoice-info.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { Component, Input } from "@angular/core";
+import { AlertDialogComponent } from "../alert-dialog/alert-dialog.component";
 
 @Component({
   selector: "app-invoice-info",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AlertDialogComponent],
   templateUrl: "./invoice-info.component.html",
 })
 export class InvoiceInfoComponent {
