1405df7ed76974ac0424de8bd283d5073b2fa95e
thulanigamtee <thulanigamtee@gmail.com>
2024-11-12 22:37:00 +0200

    configured animation for router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { SidebarComponent } from "./components/shared/sidebar/sidebar.component";
+import { fadeIn } from "./animations/route-animation";
 
 @Component({
   selector: "app-root",
   standalone: true,
   imports: [RouterOutlet, SidebarComponent],
   templateUrl: "./app.component.html",
+  animations: [fadeIn]
 })
-export class AppComponent {}
+export class AppComponent {
+  getRouteAnimationData(outlet: RouterOutlet) {
+    // return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation']
+    return outlet && outlet.activatedRouteData;
+  }
+}
