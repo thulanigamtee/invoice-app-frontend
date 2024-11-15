90303dc27de734e6305279778a7a5dc87cdeb150
thulanigamtee <thulanigamtee@gmail.com>
2024-11-15 23:50:29 +0200

    created service to observe break points

---
+++ src/app/services/breakpointObserver.service.ts
@@ -0,0 +1,21 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BreakpointObserverService {
+  public isMediumWidth = new BehaviorSubject<boolean>(false);
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+  
+  observeBreakpoint() {
+    this.breakpointObserver
+    .observe("(min-width: 768px)")
+    .pipe(map((result) => result.matches))
+    .subscribe((matches) => {
+      this.isMediumWidth.next(matches);
+  });
+  }
+}
