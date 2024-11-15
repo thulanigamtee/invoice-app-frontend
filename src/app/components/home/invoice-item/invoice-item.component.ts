3cec8127d9f9a36aee155d0d8bfd3328027bce85
thulanigamtee <thulanigamtee@gmail.com>
2024-11-15 23:54:01 +0200

    added conditional rendering for loader/no invoices component

--- src/app/components/home/invoice-item/invoice-item.component.ts
+++ src/app/components/home/invoice-item/invoice-item.component.ts
@@ -4,21 +4,39 @@ import { CommonModule } from "@angular/common";
 import { RouterLink } from "@angular/router";
 import { StatusIndicatorComponent } from "../../shared/status-indicator/status-indicator.component";
 import { InvoiceService } from "../../../services/invoice.service";
+import { NoInvoicesComponent } from "../no-invoices/no-invoices.component";
+import { LoaderComponent } from "../../shared/loader/loader.component";
 
 @Component({
   selector: "app-invoice-item",
   standalone: true,
-  imports: [CommonModule, RouterLink, StatusIndicatorComponent],
+  imports: [CommonModule, RouterLink, StatusIndicatorComponent, NoInvoicesComponent, LoaderComponent],
   templateUrl: "./invoice-item.component.html",
 })
 export class InvoiceItemComponent {
   invoices: Invoice[] = [];
+  invoicesCount:number = 0;
+  isLoading: boolean = true;
 
   constructor(private invoiceService: InvoiceService) {}
 
   ngOnInit(): void {
+    this.getInvoices();
+  }
+
+  getInvoices() {
     this.invoiceService.getInvoices().subscribe((data) => {
       this.invoices = data;
-    });
+      this.invoicesCount = data.length;
+      this.isLoading = false;
+    })
+  }
+
+  filterByStatus(status: string) { 
+    this.invoiceService.getInvoices().subscribe((data) => {
+      this.invoices = data.filter((invoice) => {
+        return invoice.status === status;
+      })
+    })
   }
 }
