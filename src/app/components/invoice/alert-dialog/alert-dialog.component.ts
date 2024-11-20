fb35b3ef02a81f013bc2168323aaa836c6808af9
thulanigamtee <thulanigamtee@gmail.com>
2024-11-20 16:26:06 +0200

    implemented takeUntil for subscriptions

--- src/app/components/invoice/alert-dialog/alert-dialog.component.ts
+++ src/app/components/invoice/alert-dialog/alert-dialog.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { ButtonComponent } from "../../shared/button/button.component";
 import { InvoiceService } from "../../../services/invoice.service";
 import { DialogService } from "../../../services/dialog.service";
 import { Router } from "@angular/router";
+import { Subject, takeUntil } from "rxjs";
 
 @Component({
   selector: "app-alert-dialog",
@@ -10,8 +11,10 @@ import { Router } from "@angular/router";
   imports: [ButtonComponent],
   templateUrl: "./alert-dialog.component.html",
 })
-export class AlertDialogComponent {
-  @Input() invoiceId: string = "";
+export class AlertDialogComponent implements OnInit {
+  @Input() invoiceId!: string;
+  dialogState!: boolean;
+  private destroy$ = new Subject<void>();
 
   constructor(
     private invoiceService: InvoiceService,
@@ -19,16 +22,30 @@ export class AlertDialogComponent {
     private router: Router,
   ) {}
 
-  dialogState(): boolean {
-    return this.dialogService.dialogState;
+  ngOnInit() {
+    this.dialogService.isActive$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((state) => {
+        this.dialogState = state;
+      });
   }
 
   cancelDeletion(): void {
     this.dialogService.dialogState = false;
   }
   confirmDeletion(): void {
-    this.invoiceService.deleteInvoice(this.invoiceId).subscribe();
+    this.invoiceService
+      .deleteInvoice(this.invoiceId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe();
+    console.log("dialog init");
+
     this.dialogService.dialogState = false;
     this.router.navigate(["/"]);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
