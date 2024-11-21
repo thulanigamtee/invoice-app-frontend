82187e9e442b09494275afd1247947b8c72c477d
thulanigamtee <thulanigamtee@gmail.com>
2024-11-21 17:58:14 +0200

    variables to keep track of toast state and toast message

--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ToastService {
+  private isActive = new BehaviorSubject<boolean>(false);
+  isActive$ = this.isActive.asObservable();
 
-  constructor() { }
+  private _message = new BehaviorSubject<string>("");
+  message$ = this._message.asObservable();
+
+  get toastState() {
+    return this.isActive.value;
+  }
+
+  set toastState(state: boolean) {
+    this.isActive.next(state);
+  }
+
+  get message() {
+    return this._message.value;
+  }
+
+  set message(message: string) {
+    this._message.next(message);
+  }
 }
