0348912317ccfd0f5f07e776d3b1c40eb4eb0b65
thulanigamtee <thulanigamtee@gmail.com>
2024-11-21 11:57:00 +0200

    imported toast component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { SidebarComponent } from "./components/shared/sidebar/sidebar.component";
 import { fadeIn } from "./animations/route-animation";
+import { ToastComponent } from "./components/shared/toast/toast.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, ToastComponent],
   templateUrl: "./app.component.html",
-  animations: [fadeIn]
+  animations: [fadeIn],
 })
 export class AppComponent {
   getRouteAnimationData(outlet: RouterOutlet) {
