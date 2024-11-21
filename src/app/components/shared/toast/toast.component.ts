b0928f8df4c4e36f4dad892130da151974e68986
thulanigamtee <thulanigamtee@gmail.com>
2024-11-21 17:55:15 +0200

    implemented toast state and toast message subscriptions

--- src/app/components/shared/toast/toast.component.ts
+++ src/app/components/shared/toast/toast.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ToastService } from "../../../services/toast.service";
+import { Subject, takeUntil } from "rxjs";
 
 @Component({
-  selector: 'app-toast',
+  selector: "app-toast",
   standalone: true,
   imports: [],
-  templateUrl: './toast.component.html',
-  styleUrl: './toast.component.scss'
+  templateUrl: "./toast.component.html",
 })
 export class ToastComponent {
+  toastState: boolean = false;
+  message: string = "";
+  private destroy$ = new Subject<void>();
 
+  constructor(private toastService: ToastService) {}
+
+  ngOnInit() {
+    this.toastService.isActive$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((state) => {
+        this.toastState = state;
+      });
+    this.toastService.message$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((message) => {
+        this.message = message;
+      });
+  }
 }
