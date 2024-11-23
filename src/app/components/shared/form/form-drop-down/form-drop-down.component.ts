03694f37e942cd3272d0b791af1cf1256ca6ca75
thulanigamtee <thulanigamtee@gmail.com>
2024-11-23 02:59:54 +0200

    implemented subscription for payment term

--- src/app/components/shared/form/form-drop-down/form-drop-down.component.ts
+++ src/app/components/shared/form/form-drop-down/form-drop-down.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormService } from "../../../../services/form.service";
+import { Subject, takeUntil } from "rxjs";
 
 @Component({
   selector: "app-form-drop-down",
@@ -6,16 +8,39 @@ import { Component } from "@angular/core";
   imports: [],
   templateUrl: "./form-drop-down.component.html",
 })
-export class FormDropDownComponent {
+export class FormDropDownComponent implements OnInit, OnDestroy {
   isActive: boolean = false;
+  paymentTerm!: number;
+  private destroy$ = new Subject<void>();
+
+  constructor(private formService: FormService) {}
+
+  options: { id: number; value: number }[] = [
+    { id: 0, value: 1 },
+    { id: 1, value: 7 },
+    { id: 2, value: 14 },
+    { id: 3, value: 30 },
+  ];
+
+  setPaymentTerm(term: number) {
+    this.formService.paymentTerm = term;
+    this.toggleDropdown();
+  }
+
   toggleDropdown() {
     this.isActive = !this.isActive;
   }
 
-  options: { id: number; value: string; isActive: boolean }[] = [
-    { id: 0, value: "net 1 day", isActive: false },
-    { id: 1, value: "net 7 days", isActive: false },
-    { id: 2, value: "net 14 days", isActive: true },
-    { id: 3, value: "net 30 days", isActive: true },
-  ];
+  ngOnInit() {
+    this.formService.paymentTerm$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((term) => {
+        this.paymentTerm = term;
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
