1b89bfd580271b383fafc402d77cc30678ddf5a2
thulanigamtee <thulanigamtee@gmail.com>
2024-11-25 01:14:25 +0200

    implement form functionality

--- src/app/components/shared/form/form-buttons/form-buttons.component.ts
+++ src/app/components/shared/form/form-buttons/form-buttons.component.ts
@@ -1,6 +1,10 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { ButtonComponent } from "../../button/button.component";
 import { FormService } from "../../../../services/form.service";
+import { FormGroup } from "@angular/forms";
+import { InvoiceService } from "../../../../services/invoice.service";
+import { Subject, takeUntil } from "rxjs";
+import { ToastService } from "../../../../services/toast.service";
 
 @Component({
   selector: "app-form-buttons",
@@ -9,23 +13,14 @@ import { FormService } from "../../../../services/form.service";
   templateUrl: "./form-buttons.component.html",
 })
 export class FormButtonsComponent {
-  @Output() submitEvent = new EventEmitter();
-  @Output() saveChangesEvent = new EventEmitter();
-  @Output() saveAsDraftEvent = new EventEmitter();
+  @Input() form!: FormGroup;
+  private destroy$ = new Subject<void>();
 
-  emitSubmitEvent() {
-    this.submitEvent.emit();
-  }
-
-  emitSaveChangesEvent() {
-    this.saveChangesEvent.emit();
-  }
-
-  emitSaveAsDraftEvent() {
-    this.saveAsDraftEvent.emit();
-  }
-
-  constructor(private formService: FormService) {}
+  constructor(
+    private formService: FormService,
+    private invoiceService: InvoiceService,
+    private toastService: ToastService,
+  ) {}
 
   isEditMode() {
     return this.formService.isEditMode;
@@ -34,5 +29,74 @@ export class FormButtonsComponent {
   discardForm() {
     this.formService.formState = false;
     document.body.classList.remove("no-scroll");
+  }
+
+  createInvoice() {
+    this.form.get("status")?.setValue("pending");
+    this.invoiceService
+      .createInvoice(this.form.value)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: () => {
+          this.formService.formState = false;
+          document.body.classList.remove("no-scroll");
+          setTimeout(() => {
+            this.toastService.toastState = true;
+          }, 500);
+          this.toastService.message = "invoice successfully created";
+          setTimeout(() => {
+            this.toastService.toastState = false;
+          }, 2000);
+        },
+        error: () => {
+          this.toastService.message = "error creating invoice";
+        },
+      });
+  }
+
+  saveChanges() {
+    const id = this.form.get("id")?.value;
+    this.invoiceService
+      .updateInvoice(id, this.form.value)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: () => {
+          this.formService.formState = false;
+          document.body.classList.remove("no-scroll");
+          setTimeout(() => {
+            this.toastService.toastState = true;
+          }, 500);
+          this.toastService.message = "invoice successfully updated";
+          setTimeout(() => {
+            this.toastService.toastState = false;
+          }, 2000);
+        },
+        error: () => {
+          this.toastService.message = "error updating invoice";
+        },
+      });
+  }
+
+  saveAsDraft() {
+    this.form.get("status")?.setValue("draft");
+    this.invoiceService
+      .createInvoice(this.form.value)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: () => {
+          this.formService.formState = false;
+          document.body.classList.remove("no-scroll");
+          setTimeout(() => {
+            this.toastService.toastState = true;
+          }, 500);
+          this.toastService.message = "draft successfully created";
+          setTimeout(() => {
+            this.toastService.toastState = false;
+          }, 2000);
+        },
+        error: () => {
+          this.toastService.message = "error creating invoice";
+        },
+      });
   }
 }
