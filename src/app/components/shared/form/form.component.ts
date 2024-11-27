6a708ae69ce1dbdb3b1267b72560baac9d1d7a9d
thulanigamtee <thulanigamtee@gmail.com>
2024-11-27 17:52:59 +0200

    remove created at initial value

--- src/app/components/shared/form/form.component.ts
+++ src/app/components/shared/form/form.component.ts
@@ -70,7 +70,7 @@ export class FormComponent implements OnInit, OnDestroy {
   initialiseForm() {
     this.invoiceForm = this.formBuilder.group({
       id: [""],
-      createdAt: [new Date(), Validators.required],
+      createdAt: ["", Validators.required],
       paymentDue: ["", Validators.required],
       description: ["", Validators.required],
       paymentTerms: [0, Validators.required],
